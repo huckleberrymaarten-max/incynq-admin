153eb4a2d5042e2d5eb654fad15213a3e3fad484
Maarten <huckleberrymaarten@gmail.com>
2026-04-23 15:37:00 +0100

    Fix: Use correct Supabase anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://muzzjvegynsemlsbwggf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11enpqdmVneW5zZW1sc2J3Z2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MTI0NTQsImV4cCI6MjA1MTM4ODQ1NH0.3ilk8J4sDq4tXl8KPRK_MqTIuWcYnhO0Tbb1p-Tw0gc'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11enpqdmVneW5zZW1sc2J3Z2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMjI5MDIsImV4cCI6MjA5MTU5ODkwMn0.AO2BgiecEKZvqCeZyJPZrS7AhOI6UsZTMHMvfXyAaXI'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
